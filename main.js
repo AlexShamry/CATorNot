c283ba9eb5ac1225030b170988645542a5e5f6e0
AlexShamry <162981732+AlexShamry@users.noreply.github.com>
2024-03-28 15:33:16 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-const TheButton = document.querySelector("button")
-const TheImage = document.querySelector("img");
+const TheButton = document.querySelector("button");
+const Welcome = document.querySelector("h1");
+const ChangeButton = document.getElementById("change");
+function setUserName(){
+    let myName = prompt("Please, enter your name!");
+    if(!myName){
+        setUserNameArg("user")
+    }
+    else {
+        localStorage.setItem("name", myName);
+        Welcome.textContent = `welcome, ${myName}!`;
+    }
+}
+function setUserNameArg(name){
+    localStorage.setItem("name", name);
+    Welcome.textContent = `welcome, ${name}!`;
+}
+if(!localStorage.getItem("name")){
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    Welcome.textContent = `Welcome, ${storedName}!`;
+}
 TheButton.onclick = () => {
+    const TheImage = document.querySelector("img");
     const ImgSrc = TheImage.getAttribute("src");
     if (ImgSrc === "images/cat.png"){
         TheImage.setAttribute("src", "images/rick.gif")
@@ -9,4 +31,7 @@ TheButton.onclick = () => {
         TheImage.setAttribute("src", "images/cat.png")
         TheButton.textContent = "Is it a cat?"
     }
+}
+ChangeButton.onclick = () => {
+    setUserName()
 }
